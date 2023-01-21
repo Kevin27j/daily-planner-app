e60253d9f6ce8a8e35fa2b380290c21dbfab5200
Kejvin Pashaj <kejvinpashaj@Kejvins-Air.broadband>
2023-01-21 12:00:55 +0000

    timeblock element starter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,47 @@
-let currentDay = $("#currentDay");
-let timeBlockContainer = $(".container");
 // use DOM manipulation
 // integration of third-party API
 // jQuery
 // moment.js
 
 // ******************************************************
+// currentDay ID element
+let currentDay = $("#currentDay");
+// timeblocks element container
+let timeBlockContainer = $(".container");
 
-// create a day planner app
 // get the current day and display it to the screen
 let momentDate = moment();
 currentDay.text(momentDate.format("dddd, MMMM Do, HH:mm"))
     
-// based on current date and time display a list of timetable
-    // each has 1hour
-    // where i can input a text 
-    // and save it to local storage
-
 // make a function to render timeblocks
-function renderBlocks(){
-    // create time block list based on css classes
+function renderTimeBlocks(){
+    // initialize variable for the current hour
+    let currentHour = moment().format("HH");
+
+    // create timeBlock <ul> element with class of time-block
+    let timeBlock = $("<ul>").attr("class", "time-block");
+    // and append to timeBlock container
+    timeBlockContainer.append(timeBlock);
+
+    // create a loop to display a 9am to 5 pm timeblock
+    for (let i = 9; i < 18; i++){
+        // create row element <li> with class of row
+        let row = $("<li>").attr("class", "row");
+        // append to timeBlock <ul>
+        timeBlock.append(row);
+        
+        // create hour element <div> with class of hour
+        let hour = $("<div>").attr("class", "hour");
+        // assign textContent of loop i
+        hour.text(i)
+        // append to each row <li>
+        row.append(hour);
+    }
+
     // use moment.js to display blocks based on the current day time
-    // initialize a var to hold and create time block elements
-    let timeBlock = $("<>")
 }
+
+renderTimeBlocks();
 
 // each timeblock has a different color based on the current time
     // grey for the past
