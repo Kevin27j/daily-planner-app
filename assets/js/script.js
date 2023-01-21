02630cec8c0da31f6ad310a1157d5214f41c20f4
Kejvin Pashaj <kejvinpashaj@Kejvins-Air.broadband>
2023-01-21 13:12:19 +0000

    timeblock current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,23 +19,50 @@ function renderTimeBlocks(){
     let currentHour = moment().format("HH");
 
     // create timeBlock <ul> element with class of time-block
-    let timeBlock = $("<ul>").attr("class", "time-block");
+    let timeBlock = $("<div>").attr("class", "time-block");
     // and append to timeBlock container
     timeBlockContainer.append(timeBlock);
 
+    let hourBlocks = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
     // create a loop to display a 9am to 5 pm timeblock
-    for (let i = 9; i < 18; i++){
+    for (let i = 0; i < hourBlocks.length; i++){
+        // ROW LIST
         // create row element <li> with class of row
-        let row = $("<li>").attr("class", "row");
+        let row = $("<div>").attr("class", "row");
         // append to timeBlock <ul>
         timeBlock.append(row);
         
-        // create hour element <div> with class of hour
+        // HOUR DIV
+        // create hour element for each block
         let hour = $("<div>").attr("class", "hour");
         // assign textContent of loop i
-        hour.text(i)
+        hour.text(hourBlocks[i])
         // append to each row <li>
         row.append(hour);
+
+        // INPUT DIV
+        // create input text for each block
+        let input = $("<textarea>");
+        row.append(input);
+
+        // BUTTON DIV
+        // create save button for each block
+        let button = $("<button>").attr("class", "saveBtn");
+        row.append(button);
+
+        // if-else conditional to check current hour to time-block hour
+        // and assign class based on
+        if (hourBlocks[i] < currentHour){
+            input.attr("class", "past");
+        }
+        else if (hourBlocks[i] > currentHour){
+            input.attr("class", "future");
+        } 
+        else {
+            input.attr("class", "present");
+        }
+            
     }
 
     // use moment.js to display blocks based on the current day time
@@ -44,7 +71,6 @@ function renderTimeBlocks(){
 renderTimeBlocks();
 
 // each timeblock has a different color based on the current time
-    // grey for the past
     // red for the present 
     // green for the future
 
